8cc637467c42f9111073e84855d0548ef78941bc
caojianbo <caojianbo@taptap.com>
2020-12-15 14:28:39 +0800

    change dependency version

--- app/src/main/java/com/tds/demo/MainActivity.java
+++ app/src/main/java/com/tds/demo/MainActivity.java
@@ -1,37 +1,32 @@
 package com.tds.demo;
 
-import android.content.Intent;
 import android.util.Log;
 import android.view.View;
-import androidx.annotation.Nullable;
+import android.webkit.WebView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.taptap.sdk.AccessToken;
-import com.taptap.sdk.CallBackManager;
-import com.taptap.sdk.LoginManager;
-import com.taptap.sdk.LoginResponse;
 import com.taptap.sdk.Profile;
-import com.taptap.sdk.TapTapLoginCallback;
 import com.taptap.sdk.TapTapSdk;
 import com.taptap.sdk.helper.TapLoginHelper;
+import com.taptap.sdk.helper.TapLoginHelper.TapLoginResultCallback;
 import com.taptap.sdk.net.Api.ApiCallback;
 import com.tds.TdsInitializer;
+import com.tds.common.net.error.AccountGlobalError;
 import com.tds.moment.TapTapMomentSdk;
 import com.tds.moment.TapTapMomentSdk.Config;
 import com.tds.moment.TapTapMomentSdk.TapMomentCallback;
-import com.tds.tapdb.sdk.LoginType;
-import com.tds.tapdb.sdk.TapDB;
-import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
 
   private static final String Tag = "MainActivity";
 
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    WebView.setWebContentsDebuggingEnabled(true);
 
     com.tds.TdsConfig.Builder configBuilder = new com.tds.TdsConfig.Builder()
         .appContext(getApplicationContext())
@@ -39,58 +34,66 @@ public class MainActivity extends AppCompatActivity {
 
     TdsInitializer.init(configBuilder.build());
     TdsInitializer.enableTapDB(this, "1.0", "default");
+
+    // 开启动态
     TdsInitializer.enableMoment(this);
+    TapTapMomentSdk.setHandleLoginResult(true);
 
-
-    TapLoginHelper.getInstance().setLoginResultCallback(new TapLoginHelper.ITapLoginResultCallback() {
+    TdsInitializer.registerLoginCallback(new TapLoginResultCallback() {
       @Override
       public void onLoginSuccess(AccessToken accessToken) {
-        Log.e(Tag, "onLoginSuccess");
-        Profile.fetchProfileForCurrentAccessToken(new ApiCallback<Profile>() {
-          @Override
-          public void onSuccess(Profile profile) {
-            TapDB.setUser(profile.getOpenid(), profile.getOpenid(), LoginType.TapTap);
-          }
-
-          @Override
-          public void onError(Throwable throwable) {
-
-          }
-        });
-
+        Log.e("MainActivity", "onLoginSuccess" + "" + accessToken);
       }
 
       @Override
       public void onLoginCancel() {
-        Log.e(Tag, "onLoginCancel");
+        Log.e("MainActivity", "onLoginCancel" + "");
       }
 
       @Override
-      public void onLoginError(Throwable throwable) {
-        Log.e(Tag, "onLoginError");
+      public void onLoginError(AccountGlobalError accountGlobalError) {
+        Log.e("MainActivity", "onLoginError" + " " + accountGlobalError.toJsonString());
       }
     });
+
   }
 
   public void sdkLogin(View view) {
-    TapLoginHelper.getInstance().startTapLogin(MainActivity.this,TapTapSdk.SCOPE_PUIBLIC_PROFILE);
+    TapLoginHelper.startTapLogin(MainActivity.this,TapTapSdk.SCOPE_PUIBLIC_PROFILE);
   }
 
+  public void sdkLogout(View view) {
+    TapLoginHelper.logout();
+  }
 
   public void sdkMoment(View view) {
+
     Config config = new Config();
     TapTapMomentSdk.openTapMoment(config);
     TapTapMomentSdk.setCallback(new TapMomentCallback() {
       @Override
       public void onCallback(int i, String s) {
-        Log.e("MainActivity", "onCallback" + "" + i + " _______________   "  + s);
-        if (i == 60000) {
-          try {
-            TapTapMomentSdk.setLoginToken(new AccessToken(s));
-          } catch (JSONException e) {
-            e.printStackTrace();
-          }
-        }
+        Log.e("MainActivity", "onCallback moment " + "  " + i + "   " + s) ;
+      }
+    });
+  }
+
+
+  public  void getRedPoint(View view) {
+    TapTapMomentSdk.getNoticeData();
+  }
+
+  public void fetchUserProfile(View view) {
+
+    TapLoginHelper.fetchProfileForCurrentAccessToken(new ApiCallback<Profile>() {
+      @Override
+      public void onSuccess(Profile profile) {
+        Log.e("MainActivity", "onSuccess" + " " + profile);
+      }
+
+      @Override
+      public void onError(Throwable throwable) {
+        Log.e("MainActivity", "onError" + "");
       }
     });
   }
