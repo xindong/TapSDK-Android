828b5eaad02f6757c735d3b4ecf16309ef8f917a
qiankun <qiankun@xd.com>
2020-12-28 11:51:37 +0800

    mod: add db test

--- app/src/main/java/com/tds/demo/tapdb/TapDBActivity.java
+++ app/src/main/java/com/tds/demo/tapdb/TapDBActivity.java
@@ -13,85 +13,75 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.taptap.sdk.Profile;
 import com.tds.demo.R;
 import com.tds.tapdb.sdk.TapDB;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
-public class TapDBActivity extends AppCompatActivity implements TextWatcher, View.OnClickListener {
+public class TapDBActivity extends AppCompatActivity {
 
-    public final static String TAG = "TapDBActivity";
+  public final static String TAG = "TapDBActivity";
 
-    private EditText setNameValueEditTextView, setLevelValueEditTextView, setServerValueEditTextView;
-    private Button submitButton;
+  private TextView tipsTextView;
+  private EditText setNameValueEditTextView, setLevelValueEditTextView, setServerValueEditTextView;
 
-    private boolean isSubmitEnabled() {
-        return !TextUtils.isEmpty(setNameValueEditTextView.getText().toString())
-                || !TextUtils.isEmpty(setLevelValueEditTextView.getText().toString())
-                || !TextUtils.isEmpty(setServerValueEditTextView.getText().toString());
+  private void bindView() {
+    tipsTextView = findViewById(R.id.tipsTextView);
+    setNameValueEditTextView = findViewById(R.id.setNameValueEditTextView);
+    setLevelValueEditTextView = findViewById(R.id.setLevelValueEditTextView);
+    setServerValueEditTextView = findViewById(R.id.setServerValueEditTextView);
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_tapdb);
+    bindView();
+    if (Profile.getCurrentProfile() == null) {
+      tipsTextView.setText("当前未登录，请先回上级页面完成登录");
     }
+  }
 
-    private void bindView() {
-        setNameValueEditTextView = findViewById(R.id.setNameValueEditTextView);
-        setLevelValueEditTextView = findViewById(R.id.setLevelValueEditTextView);
-        setServerValueEditTextView = findViewById(R.id.setServerValueEditTextView);
-        submitButton = findViewById(R.id.submitButton);
-        setNameValueEditTextView.addTextChangedListener(this);
-        setLevelValueEditTextView.addTextChangedListener(this);
-        setServerValueEditTextView.addTextChangedListener(this);
-        submitButton.setOnClickListener(this);
+  public void setName(View view) {
+    if (!TextUtils.isEmpty(setNameValueEditTextView.getText().toString())) {
+      Log.d(TAG, "set name:" + setNameValueEditTextView.getText().toString());
+      TapDB.setName(setNameValueEditTextView.getText().toString());
     }
+  }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tapdb);
-        bindView();
-        initView();
+  public void setLevel(View view) {
+    if (!TextUtils.isEmpty(setLevelValueEditTextView.getText().toString())) {
+      try {
+        TapDB.setLevel(Integer.parseInt(setLevelValueEditTextView.getText().toString()));
+        Log.d(TAG, "set name:" + Integer.parseInt(setLevelValueEditTextView.getText().toString()));
+      } catch (NumberFormatException e) {
+        e.printStackTrace();
+        Toast.makeText(TapDBActivity.this, "只能输入蒸熟", Toast.LENGTH_SHORT).show();
+      }
     }
+  }
 
-    private void initView() {
-        submitButton.setEnabled(isSubmitEnabled());
+  public void setServer(View view) {
+    if (!TextUtils.isEmpty(setServerValueEditTextView.getText().toString())) {
+      TapDB.setServer(setServerValueEditTextView.getText().toString());
+      Log.d(TAG, "set server:" + setServerValueEditTextView.getText().toString());
     }
+  }
 
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+  public void onCharge(View view) {
+    TapDB.onCharge("111111", "product1", 2, "cny", "30");
+  }
 
+  public void onEvent(View view) {
+    JSONObject jsonObject = new JSONObject();
+    try {
+      jsonObject.put("eventCode", 22);
+      jsonObject.put("eventName", "testEvent");
+    } catch (JSONException e) {
+      e.printStackTrace();
     }
-
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-    }
-
-    @Override
-    public void afterTextChanged(Editable s) {
-        submitButton.setEnabled(isSubmitEnabled());
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.submitButton:
-                if (!TextUtils.isEmpty(setNameValueEditTextView.getText().toString())) {
-                    Log.d(TAG, "set name:" + setNameValueEditTextView.getText().toString());
-                    TapDB.setName(setNameValueEditTextView.getText().toString());
-                }
-                if (!TextUtils.isEmpty(setLevelValueEditTextView.getText().toString())) {
-                    try {
-                        TapDB.setLevel(Integer.parseInt(setLevelValueEditTextView.getText().toString()));
-                        Log.d(TAG, "set name:" + Integer.parseInt(setLevelValueEditTextView.getText().toString()));
-                    } catch (NumberFormatException e) {
-                        e.printStackTrace();
-                        Toast.makeText(TapDBActivity.this, "只能输入蒸熟", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                if (!TextUtils.isEmpty(setServerValueEditTextView.getText().toString())) {
-                    TapDB.setServer(setServerValueEditTextView.getText().toString());
-                    Log.d(TAG, "set server:" + setServerValueEditTextView.getText().toString());
-                }
-                break;
-            default:
-                break;
-        }
-    }
+    TapDB.onEvent("22", jsonObject);
+  }
 }
